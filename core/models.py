769fecadfe7d5fda523fc9f4400b392989fda318
billybautista <billy.bautista.cornelio@gmail.com>
2025-10-20 20:39:07 -0300

    Add Owner, Pet, and Appointment models with initial migration

--- core/models.py
+++ core/models.py
@@ -1,3 +1,53 @@
+from os import name
 from django.db import models
 
-# Create your models here.
+
+class Owner(models.Model):
+
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=10)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Pet(models.Model):
+
+    PET_TYPE_CHOICES = [
+        ("dog", "Dog"),
+        ("cat", "Cat"),
+        ("bird", "Bird"),
+        ("rabbit", "Rabbit"),
+        ("hamster", "Hamster"),
+        ("turtle", "Turtle"),
+        ("other", "Other"),
+    ]
+
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    breed = models.CharField(max_length=100)
+    pet_type = models.CharField(max_length=100, choices=PET_TYPE_CHOICES)
+
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE, related_name="pets")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Appointment(models.Model):
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE, related_name="appointments")
+    date = models.DateField()
+    time = models.TimeField()
+    reason = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.pet.name} - {self.date} - {self.time}"
